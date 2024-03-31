4a74a19b755653dcdd5d572630d4cc612d0ed50d
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-31 20:15:31 -0300

    feat: interoperabilidade java e Kotlin

--- app/src/main/java/com/apolowilkersilva/myfirstaplication/Mensagem.kt
+++ app/src/main/java/com/apolowilkersilva/myfirstaplication/Mensagem.kt
@@ -1,4 +1,8 @@
 package com.apolowilkersilva.myfirstaplication
-
+// Interoperabilidade
 class Mensagem {
+    fun enviarNovaMensagem() {
+        val usuario = Usuario()
+        usuario.autenticar()
+    }
 }
--- app/src/main/java/com/apolowilkersilva/myfirstaplication/Usuario.java
+++ app/src/main/java/com/apolowilkersilva/myfirstaplication/Usuario.java
@@ -1,5 +1,7 @@
 package com.apolowilkersilva.myfirstaplication;
-
+// Interoperabilidade
 public class Usuario {
-
+    public void autenticar() {
+        new Mensagem().enviarNovaMensagem();
+    }
 }
